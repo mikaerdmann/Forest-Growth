4ccd3205f021191cc9d7c4eaaec64d23d83ed56a
mikaerdmann <mikaerdmann.2@gmail.com>
2024-03-08 14:46:39 +0100

    develop model

--- src/models/functions.py
+++ src/models/functions.py
@@ -2,6 +2,7 @@
 # Mika Erdmann, Intern at Aarhus University ENVS
 # Supervisor Katarina Elofsson
 # Started 07.03.2024
+
 import numpy as np
 
 
@@ -15,21 +16,20 @@ import numpy as np
 class Params_Thomas:
     # I create an object that contains all of the parameters used in Thomas function. V_gross etc. contain all species.
     # In the first column, all variables have the names of the species.
-    def __init__(self, V_gross, V_pot, V_lim, Species_Shares ):
-        self.V_gross = V_gross[1]
-        self.V_pot = V_pot[1]
-        self.V_lim = V_lim[1]
-        self.Species_Shares = Species_Shares[1]
-        self.Species_names = Species_Shares[0]
+    def __init__(self, V_gross, V_pot, V_lim, Species_Shares, country_name):
+        self.V_gross = V_gross[1,]
+        self.V_pot = V_pot[1,]
+        self.V_lim = V_lim[1,]
+        self.Species_Shares = Species_Shares[1,]
+        self.Species_names = Species_Shares[0,]
+        self.country = country_name
 
 # TODO: Read in data as variables (maybe somewhere else?)
 
+def Growth_Thomas_t(V_t, country):
+    # create object with data for the country
+    Thomas = Params_Thomas(V_gross=, V_pot=, V_lim=, Species_Shares=, country_name=)
 
-# create object with data
-Thomas = Params_Thomas(V_gross=, V_pot=, V_lim=, Species_Shares=)
-
-
-def Growth_Thomas_t(V_t, Thomas):
     # The function calculates the growth in one country in one year
     G_t_i = np.zeros(len(Thomas.Species_Shares))
     # Now I loop over all of the species to calculate species-specific growth in one country and add it together afterward
@@ -37,5 +37,6 @@ def Growth_Thomas_t(V_t, Thomas):
         V_t_i = Thomas.Species_Shares[i] * V_t          # Here I use the shares of each species in the country
         G_t_i[i] = Thomas.V_gross[i] * (Thomas.V_pot[i] - V_t_i)/(Thomas.V_pot[i] - Thomas.V_lim[i])
     G_t = sum(G_t_i)
+    return G_t
 
 
--- src/models/simulation.py
+++ src/models/simulation.py
@@ -11,25 +11,30 @@ import numpy as np
 import scipy as sci
 
 # import functions
-import functions.py as fc
+import functions as fc
 
-def Growth(V_t, function):
+def Growth(V_t, function, country):
+    if approach == "Thomas":
+        return fc.Growth_Thomas_t(V_t, country)
 
-
-def current_stock_i(V_t):
-    V_t = V_t + Growth(V_t)
+def current_stock_i(V_t, country):
+    V_t = V_t + Growth(V_t, country)
     return V_t
 
-def stock_over_time_i(T, V0, y_0):
+def stock_over_time_i(country, T, V0, y_0):
     V_t = V0
     V_path = np.zeros(T)
     V_path[0] = V_t
     for t in range(0,T):
-        V_t = current_stock_i(V_t)
+        V_t = current_stock_i(V_t, country)
         V_path[t] = V_t
     return V_path
 
 
+
+# Set the approach
+approaches = ["Katarina2016", "Katarina2018", "Thomas"]
+approach = approaches[2]
 
 
 
