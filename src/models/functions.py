5f3c526d40554c73e49d50e3b59bd8cd61847223
mikaerdmann <mikaerdmann.2@gmail.com>
2024-03-11 10:27:52 +0100

    functions

--- src/models/functions.py
+++ src/models/functions.py
@@ -4,31 +4,49 @@
 # Started 07.03.2024
 
 import numpy as np
+import pandas as pd
 
-
+path_data = "C:\\Users\\mikae\\Documents\\Aarhus Internship\\model\\data\\raw"
 # This script includes the different forest growth functions
 
 # Thomas
 
 # Read in parameters as an object
 
-# TODO: For one country or all at the same time? (Then it would be matrices and not vectors)
 class Params_Thomas:
     # I create an object that contains all of the parameters used in Thomas function. V_gross etc. contain all species.
     # In the first column, all variables have the names of the species.
-    def __init__(self, V_gross, V_pot, V_lim, Species_Shares, country_name):
+    def __init__(self, V_gross, Species_Shares, country_name):
         self.V_gross = V_gross[1,]
-        self.V_pot = V_pot[1,]
-        self.V_lim = V_lim[1,]
+       # self.V_pot = V_pot[1,]
+       # self.V_lim = V_lim[1,]
         self.Species_Shares = Species_Shares[1,]
         self.Species_names = Species_Shares[0,]
         self.country = country_name
 
 # TODO: Read in data as variables (maybe somewhere else?)
+def read_params_one_country(country):
+    data = pd.read_excel(path_data+"\\Data Thomas.xlsx", sheet_name=1)
+    data_c = data[data["Country"] == country]
+    Species_Shares_c = data_c.loc[:,'Total':'Other']
+    # TODO: integrate the "Total" column in the functions
+    # TODO: reshape into long
+    V_gross_c = float(data_c.loc[:, 'Gross'])
+
+    return Species_Shares_c, V_gross_c
+
 
 def Growth_Thomas_t(V_t, country):
+    # This function has as input the national current stock and the name of the country. It reads the national parameters
+    # gross increment and shares
+    Species_Shares_c, V_gross_c = read_params_one_country(country)
     # create object with data for the country
-    Thomas = Params_Thomas(V_gross=, V_pot=, V_lim=, Species_Shares=, country_name=)
+    Thomas = Params_Thomas(V_gross=V_gross_c, Species_Shares=Species_Shares_c, country_name=country)
+
+    # Read in the calculated parameters V:pot and V_lim (species specific
+    data_pot_lim = pd.read_excel(path_data + "\\Data Thomas.xlsx", sheet_name=0)
+    V_pot_c =np.asarray(data_pot_lim["A", "C"])
+    V_lim_c =V_pot_c * 0.83
 
     # The function calculates the growth in one country in one year
     G_t_i = np.zeros(len(Thomas.Species_Shares))
@@ -39,4 +57,6 @@ def Growth_Thomas_t(V_t, country):
     G_t = sum(G_t_i)
     return G_t
 
+country = "Belgium"
 
+sp, Vgc = read_params_one_country(country)
