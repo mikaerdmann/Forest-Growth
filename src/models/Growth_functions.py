d349f2a4cf95fde1f62d373aede909e27ef56d46
mikaerdmann <mikaerdmann.2@gmail.com>
2024-03-12 11:49:27 +0100

    functions

--- src/models/Growth_functions.py
+++ src/models/Growth_functions.py
@@ -22,7 +22,7 @@ def read_params_one_country_Thomas(country):
     # TODO: How to deal with the "other"? Is it the rest to 100%?
     Species_Shares_c = pd.DataFrame({"Species": Species_area_c.loc[:,"Beech":"Other"].columns.tolist(), "Shares": areas_shares.reshape(8)})
     # Now I calculate the species shares of the gross increment
-    V_gross_c = float(data_c.loc[:, 'Gross'])
+    V_gross_c = float(data_c.loc[:, 'Gross'])  # TODO find a way to do this wihtout the future warning
     V_gross_species_c = pd.DataFrame(
         {"Species": Species_area_c.loc[:, "Beech":"Other"].columns.tolist(), "Gross": areas_shares.reshape(8) * V_gross_c})
     return Species_Shares_c, V_gross_c, V_gross_species_c
@@ -55,7 +55,33 @@ def Growth_Thomas_t(V_t, country):
     return G_t, G_t_i
 
 
+# Katarina 2016
 
+def Growth_Katarina2016_t(V_t, country):
+
+    data = pd.read_excel(path_data + "\\Data Katarina 2016.xlsx", sheet_name=0, decimal=',')
+
+def average_age_t():
+    return None
+
+
+# Katarina 2018
+
+def Growth_Katarina2018(V_t, country):
+    SC_list = ["Sweden", "Finland"]
+    ME_list = ["Portugal", "Spain", "France", "Italy", "Greece", "Slovenia"]
+    if country in SC_list:
+        SC = 1
+        ME = 0
+    if country in ME_list:
+        ME = 1
+        SC = 0
+    else:
+        ME = 0
+        SC = 0
+
+    G_t = 0.05881* V_t - 0.00011044* V_t**2 - 1.32664 * SC - 0.96349*ME
+    return G_t
 
 
 # test read_params_one_country()
--- src/models/simulation.py
+++ src/models/simulation.py
@@ -9,19 +9,23 @@
 
 import numpy as np
 import scipy as sci
+import pandas as pd
 
 # import functions
-import functions as fc
+import Growth_functions as fc
 
-def Growth(V_t, function, country):
+def Growth(V_t, approach, country, y= 0):
     if approach == "Thomas":
         return fc.Growth_Thomas_t(V_t, country)
-
+    if approach == "Katarina2016":
+        return fc.Growth_Katarina2016_t(V_t, country, y)
+    if approach == "Katarina2018":
+        return fc.Growth_Katarina2018(V_t,country)
 def current_stock_i(V_t, country):
-    V_t = V_t + Growth(V_t, country)
+    V_t = V_t + Growth(V_t, approach, country )
     return V_t
 
-def stock_over_time_i(country, T, V0, y_0):
+def stock_over_time_i(country, T, V0, y_0 = 0):
     V_t = V0
     V_path = np.zeros(T)
     V_path[0] = V_t
@@ -32,9 +36,45 @@ def stock_over_time_i(country, T, V0, y_0):
 
 
 
+# TODO:
+# Read in starting stocks for each country and create country loop
+
+def read_initial_values(approach):
+    # This function reads in the initial Values needed for the models. Here, these initial values are not yet
+    # consistent, this means the initial values for Volume may vary between the approaches
+    if approach == "Katarina2018":
+        data = pd.read_excel(path_data + "\\Data Katarina 2018.xlsx", sheet_name=1, decimal=',')
+    if approach == "Katarina2016":
+        data = pd.read_excel(path_data + "\\Data Katarina 2016.xlsx", sheet_name=1, decimal=',')
+    if approach == "Thomas":
+        data = pd.read_excel(path_data + "\\Data Thomas.xlsx", sheet_name=1, decimal=',')
+    # return a df with two colums: Country and Volume
+    V_0_i = data.loc[:, ["Country", "Volume"]]
+    return V_0_i
+
+def recreate_model(y_0 = 0, T = 100):
+    # This function recreates the Growth Paths with the initial values from each approaches paper. Hence, the outcomes
+    # Should be simulating the same results as reported in the papers
+    V_0_i= read_initial_values(approach=approach)
+    # Create an empty array which will be filled with each country's path. It has as columns the countrys and
+    # as rows the time steps
+    V_i = np.zeros(T, len(V_0_i))
+    # Now for each country, using the initial Volume for this country, the stock over time (volume/growth path) is generated
+    for i in range(0, len(V_0_i)):
+        c = V_0_i["Country"][i]
+        V0 = V_0_i["Volume"][i]
+        # V will be a column in the Dataframe with the column name c
+        V = stock_over_time_i(country= c,T=T, V0=V0)
+        V_i[i] = V
+    V_i = pd.DataFrame()
+    return V_i
+
+#
 # Set the approach
+path_data = "C:\\Users\\mikae\\Documents\\Aarhus Internship\\model\\data\\raw"
 approaches = ["Katarina2016", "Katarina2018", "Thomas"]
-approach = approaches[2]
+approach = approaches[1]
 
 
+V_path  = recreate_model()
 
