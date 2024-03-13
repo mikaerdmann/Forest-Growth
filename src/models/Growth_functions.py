330dd115b5b1cb5dc0251e3183ab7225c2815eba
mikaerdmann <mikaerdmann.2@gmail.com>
2024-03-13 09:58:41 +0100

    functions

--- src/models/Growth_functions.py
+++ src/models/Growth_functions.py
@@ -39,15 +39,17 @@ def read_params_one_country_Thomas(country):
     # This function reads in all of the country-specific data (species shares and gross incremement)
     data = pd.read_excel(path_data+"\\Data Thomas.xlsx", sheet_name=1, decimal=',')
     data_c = data[data["Country"] == country]
-    Species_area_c = data_c.loc[:,'Total':'Other']
-    areas_shares = np.asarray(Species_area_c.loc[:,"Beech":"Other"])/np.asarray(Species_area_c.loc[:,'Total'])
+    data_c = data_c.rename(columns={"Norway spruce": "Spruce"})
+    lst = ["Beech", "Oak", "Spruce", "Fir", "Pine"] # included in all data
+    Species_area_c = data_c.loc[:,lst]
+    Species_area_c.loc[:,"Total"] = Species_area_c.sum(axis=1)
+    areas_shares = np.asarray(Species_area_c.loc[:,lst])/np.asarray(Species_area_c.loc[:,'Total'])
     # I calulate the shares by calculating the share of each species of the Total area
-    # TODO: How to deal with the "other"? Is it the rest to 100%?
-    Species_Shares_c = pd.DataFrame({"Species": Species_area_c.loc[:,"Beech":"Other"].columns.tolist(), "Shares": areas_shares.reshape(8)})
+    Species_Shares_c = pd.DataFrame({"Species": Species_area_c.loc[:,lst].columns.tolist(), "Shares": areas_shares.reshape(5)})
     # Now I calculate the species shares of the gross increment
     V_gross_c = float(data_c.loc[:, 'Gross'])  # TODO find a way to do this wihtout the future warning
     V_gross_species_c = pd.DataFrame(
-        {"Species": Species_area_c.loc[:, "Beech":"Other"].columns.tolist(), "Gross": areas_shares.reshape(8) * V_gross_c})
+        {"Species": Species_area_c.loc[:,lst].columns.tolist(), "Gross": areas_shares.reshape(5) * V_gross_c})
     return Species_Shares_c, V_gross_c, V_gross_species_c
 
 # Growth in one timestep
@@ -66,9 +68,11 @@ def Growth_Thomas_t(V_t, country):
     # gross increment and shares
     Species_Shares_c, V_gross_c, V_gross_species_c = read_params_one_country_Thomas(country)
     # Read in the calculated parameters V:pot and V_lim (species specific)
-    # TODO: Make sure the species are sorted consistently!
     # TODO: Solve Issue with missing "Other" species in area shares and with missing species between estimated Params (V-pot) and species shares!
-    data_pot_lim = pd.read_excel(path_data + "\\Data Thomas.xlsx", sheet_name=0, decimal=',')
+    data_pot_lim_all = pd.read_excel(path_data + "\\Data Thomas.xlsx", sheet_name=0, decimal=',', index_col="Species")
+    lst = ["Beech", "Oak", "Spruce", "Fir", "Pine"]
+    data_pot_lim = data_pot_lim_all.loc[lst]    # sort and harmonize shares and species data
+    data_pot_lim = data_pot_lim.reset_index()
     V_pot_c =data_pot_lim[["Species", "A"]]
     V_lim_c =V_pot_c.copy()
     V_lim_c["Lim"] = V_lim_c["A"] * 0.83
@@ -78,13 +82,14 @@ def Growth_Thomas_t(V_t, country):
     # First, create an empty Array that will hold every species growth
     G_t_i = np.zeros(len(Species_Shares_c))
     # Now I loop over all of the species to calculate species-specific growth in one country
-    for i in range(0, len(Species_Shares_c)-1): # TODO: delete -1 when all species available
+    for i in range(0, len(Species_Shares_c)): # TODO: delete -1 when all species available
         V_t_i = Species_Shares_c["Shares"][i] * V_t          # Here I use the shares of each species in the country to get the current species volume
 
         G_t_i[i] = V_gross_species_c["Gross"][i] * (V_pot_c_array[i] - V_t_i)/(V_pot_c_array[i] - V_lim_c_array[i])
 
     # Then, I add it together to calculate national growth
     G_t = sum(G_t_i)
+    # TODO: add species in df in G_t_i
     return G_t, G_t_i
 
 
